91dd23a77d85e7e0af92300ca4101232f5598a53
Hikari <hoangvuong19991964@gmail.com>
2021-05-06 20:32:58 +0700

    nearly complete

---
+++ EasyWebAppAPI/Controllers/ForeignKeyReferenceConfigController.cs
@@ -0,0 +1,126 @@
+﻿using EasyWebApp.API.Commons;
+using EasyWebApp.API.Extensions;
+using EasyWebApp.API.Services.CustomerDbConnStrManagerSrv;
+using EasyWebApp.Data.DbContext;
+using EasyWebApp.Data.DbContextProvider;
+using EasyWebApp.Data.Entities.SystemEntities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ForeignKeyReferenceConfigController : ControllerBase
+    {
+        private readonly ICustomerDbConnStrManagerSrv _customerDbConnStrManagerSrv;
+        private readonly IDbContextProvider _provider;
+
+        public ForeignKeyReferenceConfigController(ICustomerDbConnStrManagerSrv customerDbConnStrManagerSrv,
+                                                   IDbContextProvider dbContextProvider)
+        {
+            this._customerDbConnStrManagerSrv = customerDbConnStrManagerSrv;
+            this._provider = dbContextProvider;
+        }
+
+        private async Task<CustomerDbContext> GetDbContext(string dbGuid)
+        {
+            var userGuid = User.GetId();
+            var dbConnInfo = await _customerDbConnStrManagerSrv.GetCustomerDbInfoByGuid(dbGuid, userGuid);
+
+            return _provider.GetApplicationDbContext(dbConnInfo.BuildConnStr().ToString(), dbConnInfo.DbType);
+        }
+
+        [HttpGet("GetForeignKeyConfigById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SystemTableForeingKeyConfig>> GetForeignKeyConfigById(string dbGuid, int configId)
+        {
+            var context = await GetDbContext(dbGuid);
+            var result = await context.SystemTableForeingKeyConfigs.FirstOrDefaultAsync(t => t.Id == configId);
+
+            if (result == null)
+            {
+                return CheckData<SystemTableConfig>.ItemNotFound(configId);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetAllForeignKeyConfigBySourceTableId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<SystemTableForeingKeyConfig>>> GetAllForeignKeyConfigByTableId(string dbGuid, int tableId)
+        {
+            var context = await GetDbContext(dbGuid);
+
+            var table = await context.SystemTableConfigs.FirstOrDefaultAsync(t => t.Id == tableId);
+            if (table == null)
+            {
+                return CheckData<SystemTableConfig>.ItemNotFound(tableId);
+            }
+
+
+            var result = await context.SystemTableForeingKeyConfigs.Where(t => t.SourceTableName == table.Name).ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetAllForeignKeyConfigBySourceColumnId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SystemTableForeingKeyConfig>> GetAllForeignKeyConfigByColumnId(string dbGuid, int columnId)
+        {
+            var context = await GetDbContext(dbGuid);
+
+            var column = await context.SystemTableColumnConfigs.FirstOrDefaultAsync(t => t.Id == columnId);
+            if (column == null)
+            {
+                return CheckData<SystemTableColumnConfig>.ItemNotFound(columnId);
+            }
+
+
+            var result = await context.SystemTableForeingKeyConfigs.FirstOrDefaultAsync(t => t.SourceColumnName == column.Name);
+
+            return Ok(result);
+        }
+
+        [HttpPut("UpdateForeignKeyConfig")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SystemTableForeingKeyConfig>> UpdateForeignKeyConfig(string dbGuid, 
+                                                                                            [FromBody] SystemTableForeingKeyConfig updatedConfig)
+        {
+            var context = await GetDbContext(dbGuid);
+
+            var result = await context.SystemTableForeingKeyConfigs.FirstOrDefaultAsync(t => t.Id == updatedConfig.Id);
+            if (result == null)
+            {
+                return CheckData<SystemTableForeingKeyConfig>.ItemNotFound(updatedConfig.Id);
+            }
+
+            result.MappedRefrencedColumnName = updatedConfig.MappedRefrencedColumnName;
+            result.MappedRefrencedColumnOrdinalPos = updatedConfig.MappedRefrencedColumnOrdinalPos;
+            result.ModifiedDate = DateTime.UtcNow;
+
+            await context.SaveChangesAsync();
+            return result;
+        }
+
+    }
+}
---
+++ EasyWebAppAPI/Controllers/MasterConfigController.cs
@@ -0,0 +1,120 @@
+﻿using EasyWebApp.API.Commons;
+using EasyWebApp.API.Extensions;
+using EasyWebApp.API.Services.CustomerDbConnStrManagerSrv;
+using EasyWebApp.Data.DbContext;
+using EasyWebApp.Data.DbContextProvider;
+using EasyWebApp.Data.Entities.SystemEntities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MasterConfigController : ControllerBase
+    {
+        private readonly ICustomerDbConnStrManagerSrv _customerDbConnStrManagerSrv;
+        private readonly IDbContextProvider _provider;
+
+        public MasterConfigController(ICustomerDbConnStrManagerSrv customerDbConnStrManagerSrv,
+                                      IDbContextProvider dbContextProvider)
+        {
+            this._customerDbConnStrManagerSrv = customerDbConnStrManagerSrv;
+            this._provider = dbContextProvider;
+        }
+
+        private async Task<CustomerDbContext> GetDbContext(string dbGuid)
+        {
+            var userGuid = User.GetId();
+            var dbConnInfo = await _customerDbConnStrManagerSrv.GetCustomerDbInfoByGuid(dbGuid, userGuid);
+
+            return _provider.GetApplicationDbContext(dbConnInfo.BuildConnStr().ToString(), dbConnInfo.DbType);
+        }
+
+        [HttpGet("GetAllMasterConfig")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<SystemMasterConfig>>> GetAllMasterConfig(string dbGuid)
+        {
+            var context = await GetDbContext(dbGuid);
+            var results = await context.SystemMasterConfigs.ToListAsync();
+            return Ok(results);
+        }
+
+        [HttpGet("GetMasterConfigById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SystemMasterConfig>> GetMasterConfigById(string dbGuid, int configId)
+        {
+            var context = await GetDbContext(dbGuid);
+            var results = await context.SystemMasterConfigs.FirstOrDefaultAsync(t => t.Id == configId);
+
+            if (results == null)
+            {
+                return CheckData<SystemMasterConfig>.ItemNotFound(configId);
+            }
+
+            return Ok(results);
+        }
+
+        [HttpPost("CreateMasterConfig")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SystemMasterConfig>> CreateMasterConfig(string dbGuid, [FromBody] SystemMasterConfig newMasterConfig)
+        {
+            var context = await GetDbContext(dbGuid);
+            var exsited = await context.SystemMasterConfigs.FirstOrDefaultAsync(t => t.ConfigName == newMasterConfig.ConfigName);
+
+            if(exsited != null)
+            {
+                return CheckData<SystemMasterConfig>.ItemStringExists("ConfigName", newMasterConfig.ConfigName);
+            }
+
+            newMasterConfig.CreatedDate = DateTime.UtcNow;
+            newMasterConfig.ModifiedDate = DateTime.UtcNow;
+            newMasterConfig.IsActive = true;
+
+            await context.SystemMasterConfigs.AddAsync(newMasterConfig);
+            await context.SaveChangesAsync();
+
+            return Created("", newMasterConfig);
+        }
+
+        [HttpPut("UpdateMasterConfig")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<SystemMasterConfig>> UpdateTableConfig(string dbGuid, SystemMasterConfig updatedConfig)
+        {
+            var context = await GetDbContext(dbGuid);
+            var results = await context.SystemMasterConfigs.FirstOrDefaultAsync(t => t.Id == updatedConfig.Id);
+
+            if (results == null)
+            {
+                return CheckData<SystemMasterConfig>.ItemNotFound(updatedConfig.Id);
+            }
+
+            results.ConfigName = updatedConfig.ConfigName;
+            results.ConfigValue = updatedConfig.ConfigValue;
+            results.IsActive = updatedConfig.IsActive;
+            results.ModifiedDate = DateTime.UtcNow;
+
+            await context.SaveChangesAsync();
+            return Ok(results);
+        }
+    }
+}
